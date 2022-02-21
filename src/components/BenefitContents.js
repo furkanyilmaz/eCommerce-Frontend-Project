234a6e672a9bbd0cb24a817b05afe2d05d14d026
Furkan <furkanyilmaz-@hotmail.com>
2022-02-21 14:46:47 +0300

    BenefitContents completed

---
+++ src/components/BenefitContents.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Delivery from "../images/Delivery.png"
+import Triangular from "../images/Triangular.png"
+import Clipboard from "../images/Clipboard.png"
+
+export default function Cards() {
+  return <div className="bg-brownish h-s5 text-white">
+    <div className="flex w-fw mx-auto">
+      
+    <div className="mr-24 flex items-center h-s5 w-full">
+      <img className="mr-8" src={Delivery}></img>
+      <div className="flex flex-col items-start">
+        <p className="font-bold text-lg">FOCUS</p>
+        <p>
+        Our unwavering focus on superior service delivery and building </p>
+        
+      </div>
+    </div>
+
+    <div className="mr-24 flex items-center h-s5 w-full">
+      <img className="mr-8" src={Triangular}></img>
+      <div className="flex flex-col items-start">
+        <p className="font-bold text-lg">METHOD</p>
+        <p>
+        A standardized methodology designed to deliver measurable busin </p>
+        
+      </div>
+    </div>
+
+    <div className="flex items-center h-s5 w-full">
+      <img className="mr-8" src={Clipboard}></img>
+      <div className="flex flex-col items-start">
+        <p className="font-bold text-lg">KNOWLEDGE</p>
+        <p>
+        A highly skilled, proactive workforce that reliably improves ea </p>
+        
+      </div>
+    </div>
+
+    </div>
+  </div>;
+}
